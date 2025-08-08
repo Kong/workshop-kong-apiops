251571c94e7a41accacc6536b420920e7a8ee039
cacquaviva <claudio.acquaviva@gmail.com>
2025-08-08 15:51:43 -0300

    restart

--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -1,14 +1,14 @@
 var relearn_searchindex = [
   {
     "breadcrumb": "",
-    "content": "Introduction Kong Konnect is an API lifecycle management platform delivered as a service. The management plane is hosted in the cloud by Kong, while the runtime environments are deployed in your AWS accounts. Management plane enables customers to securely execute API management activities such as create API routes, define services etc. Runtime environments connect with the management plane using mutual transport layer authentication (mTLS), receive the updates and take customer facing API traffic.\nLearning Objectives In this workshop, you will:\nGet an architectural overview of Kong Konnect platform.\nSet up Konnect runtime on Amazon Elastic Kubernetes Service (EKS).\nLearn what are services, routes and plugin.\nDeploy a sample microservice and access the application using the defined route.\nUse the platform to address the following API Gateway use cases\nProxy caching Authentication and Authorization Response Transformer Request Callout Rate limiting And the following AI Gateway use cases\nPrompt Engineering LLM-based Request and Reponse transformation Semantic Caching Token-based Rate Limiting Semantic Routing RAG - Retrieval-Augmented Generation Expected Duration Pre-Requisite Environment Setup (20 minutes) Architectural Walkthrough (10 minutes) Sample Application and addressing the use cases (60 minutes) Next Steps and Cleanup (5 min)",
-    "description": "Introduction Kong Konnect is an API lifecycle management platform delivered as a service. The management plane is hosted in the cloud by Kong, while the runtime environments are deployed in your AWS accounts. Management plane enables customers to securely execute API management activities such as create API routes, define services etc. Runtime environments connect with the management plane using mutual transport layer authentication (mTLS), receive the updates and take customer facing API traffic.",
+    "content": "Introduction Insomnia\nLearning Objectives In this workshop, you will:\nGet an architectural overview of Kong Konnect platform.\nSet up Konnect runtime on Amazon Elastic Kubernetes Service (EKS).\nLearn what are services, routes and plugin.\nDeploy a sample microservice and access the application using the defined route.\nUse the platform to address the following API Gateway use cases\nProxy caching Authentication and Authorization Response Transformer Request Callout Rate limiting And the following AI Gateway use cases\nPrompt Engineering LLM-based Request and Reponse transformation Semantic Caching Token-based Rate Limiting Semantic Routing RAG - Retrieval-Augmented Generation Expected Duration Pre-Requisite Environment Setup (20 minutes) Architectural Walkthrough (10 minutes) Sample Application and addressing the use cases (60 minutes) Next Steps and Cleanup (5 min)",
+    "description": "Introduction Insomnia\nLearning Objectives In this workshop, you will:\nGet an architectural overview of Kong Konnect platform.\nSet up Konnect runtime on Amazon Elastic Kubernetes Service (EKS).\nLearn what are services, routes and plugin.\nDeploy a sample microservice and access the application using the defined route.\nUse the platform to address the following API Gateway use cases\nProxy caching Authentication and Authorization Response Transformer Request Callout Rate limiting And the following AI Gateway use cases",
     "tags": [],
-    "title": "API Management with Kong Konnect",
+    "title": "APIOps with Kong Konnect and Insomnia",
     "uri": "/index.html"
   },
   {
-    "breadcrumb": "API Management with Kong Konnect",
+    "breadcrumb": "APIOps with Kong Konnect and Insomnia",
     "content": "",
     "description": "",
     "tags": [],
@@ -16,7 +16,7 @@ var relearn_searchindex = [
     "uri": "/categories/index.html"
   },
   {
-    "breadcrumb": "API Management with Kong Konnect",
+    "breadcrumb": "APIOps with Kong Konnect and Insomnia",
     "content": "",
     "description": "",
     "tags": [],
